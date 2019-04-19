140cd1896ae2039c36611e2d33912a1dcc5e784b
laurih <lauri@lauhamedia.fi>
2019-04-19 12:58:36 +0300

    Basic version

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const maxNumbers = 75,
+          numbers = _.chunk(
+            [...Array(maxNumbers).keys()].map(k => k + 1),
+            maxNumbers / 5
+          ),
+          cardCount = 30;
+
+    let cards = Array(cardCount).fill();
+
+    cards = cards.map(card => {
+        let rows = Array(5).fill(),
+            usedNumbers = [];
+
+        return rows.map(row => {
+          let newRow = [];
+
+          numbers.forEach(chunk => {
+            let randomChoice = _.sample(chunk);
+            while (usedNumbers.includes(randomChoice)) {
+              randomChoice = _.sample(chunk);
+            }
+            usedNumbers.push(randomChoice);
+            newRow.push(randomChoice);
+          });
+
+          return newRow;
+        });
+
+    });
+
+    this.state = {
+      // Card is a 2d array containing the numbers
+      cards: cards,
+      maxNumbers: maxNumbers,
+      numbers: numbers,
+    };
+  }
+
   render() {
+    const { cards } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {cards.map(card => <Card rows={card} />)}
       </div>
     );
   }
 }
+
+const Card = ({ rows }) => {
+  const block = text => <div className="block"><span>{text}</span></div>;
+  return (
+    <div className="card">
+      <div className="row">
+        {'BINGO'.split('').map(letter => block(letter))}
+      </div>
+      {
+        rows.map(row => {
+          return (
+            <div className="row">
+              {row.map(num => block(num))}
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+};
 
 export default App;
